c6b2b1f16e1454eb746cdd742d1a06500dcb5ee1
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2021-11-07 15:11:11 +0300

    .

--- src/User.js
+++ src/User.js
@@ -3,7 +3,7 @@ import React from 'react';
 const User = (props) => {
   return (
     <div className="user">
-      <img className="userimage" src={props.image_url} />
+      <img className="userimage" src={props.image_url} alt={props.user_name} />
       <p className="user-name">{props.user_name}</p>
     </div>
   );
